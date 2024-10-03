02995c6fe0cdf35162feac1407a5b2cb7dcdd3b7
suthakaranburaj <suthakaranburaj@gmail.com>
2024-10-03 20:43:24 +0530

    Largest Element in an Array solved

--- Arrays/Easy/1.cpp
+++ Arrays/Easy/1.cpp
@@ -1,14 +1,27 @@
 //Largest Element in an Array
 
-#include <iostream>
-#include<vector>
-#include<math.h>
-#include<algorithm>
-#include <unordered_map>
-#include<set>
-using namespace std;
-int main(){
+#include <bits/stdc++.h>
 
-    
+using namespace std;
+int findLargestElement(int arr[], int n) {
+
+int max = arr[0];
+for (int i = 0; i < n; i++) {
+if (max < arr[i]) {
+    max = arr[i];
+}
+}
+return max;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i =0 ;i<n;i++){
+        cin>>arr[i];
+    }
+    int ans = findLargestElement(arr,n);
+    cout<<ans;
     return 0;
+
 }
