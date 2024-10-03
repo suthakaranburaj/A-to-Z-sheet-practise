0965e213c135bad288590b3f79d3d69f020603ec
suthakaranburaj <suthakaranburaj@gmail.com>
2024-10-03 21:52:44 +0530

    Second Largest Element in an Array without sorting, Move Zeros to end, Linear Search

---
+++ Arrays/Easy/3.cpp
@@ -0,0 +1,1 @@
+//Check if the array is sorted
---
+++ Arrays/Easy/7.cpp
@@ -0,0 +1,18 @@
+//zeros to end
+#include <vector>
+#include<iostream>
+#include <math.h>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] != 0) {
+                swap(nums[right], nums[left]);
+                left++;
+            }
+        }        
+    }
+};
